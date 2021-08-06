734512cbd3d086ac78381b16ec11b5da4ab13ec5
Prarthana-git <prarthanachaudhari@gmail.com>
2021-08-06 17:18:29 +0530

    Added controller functions

--- server.js
+++ server.js
@@ -1,30 +1,38 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+
+// create express app
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(bodyParser.json())
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }))
 
+// parse requests of content-type - application/json
+app.use(express.json())
+
+// Configuring the database
 const dbConfig = require('./config/database.config.js');
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
+// Connecting to the database
 mongoose.connect(dbConfig.url, {
-   useNewUrlParser:true
+    useNewUrlParser: true
 }).then(() => {
-   console.log("Successfully connected to the database");
+    console.log("Successfully connected to the database");    
 }).catch(err => {
-   console.log('could not connect to the database.Exiting now...',err);
-   process.exit();
-});   
-
-
-app.get('/',(req,res) => {
-res.json({"message":"Welcome to EasyNotes application. Take notes quickly.Organize and keep track of all your notes."});
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
 });
 
+// define a simple route
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+});
+// Require Notes routes
 require('./app/routes/note.routes.js')(app);
-app.listen(3000,() =>{
-console.log("Server is listening on port 3000");
+
+// listen for requests
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
 });
